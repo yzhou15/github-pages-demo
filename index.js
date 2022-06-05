e8b8aeeb6ead63777ab14df62151f093cfbb4644
zhouyi <zhouyi@qixiangyun.com>
2022-06-06 00:40:58 +0800

    添加button

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 window.onload = function() {
   document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
 }
+
+function myFunction(){
+	// document.getElementById("demo").innerHTML="Hello World";
+	document.getElementById("demo").innerHTML=Get();
+
+}
+
+function Get(){
+  let xmlhttp;
+  let Mac,Sn,DeviceId 
+  if (window.XMLHttpRequest)
+  {
+      //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+      xmlhttp=new XMLHttpRequest();
+  }
+  else
+  {
+      // IE6, IE5 浏览器执行代码
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function()
+  {
+      if (xmlhttp.readyState==4 && xmlhttp.status==200)
+      {
+          document.getElementById("demo").innerHTML=xmlhttp.responseText;
+      }
+  }
+  xmlhttp.open("GET","http://httpbin.org/get",true);
+  xmlhttp.send();
+
+
+  
+}
